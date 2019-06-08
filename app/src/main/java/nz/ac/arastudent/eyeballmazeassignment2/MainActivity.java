525b12877a9afddaad505515ecf2d1c920e0dda0
liam5liam <liam.brydon@live.com.au>
2019-06-08 21:49:47 +1200

    Made menu bar, got buttons layed out

--- app/src/main/java/nz/ac/arastudent/eyeballmazeassignment2/MainActivity.java
+++ app/src/main/java/nz/ac/arastudent/eyeballmazeassignment2/MainActivity.java
@@ -1,7 +1,11 @@
 package nz.ac.arastudent.eyeballmazeassignment2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,40 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Set tool bar
+        Toolbar myToolbar = findViewById(R.id.game_toolbar);
+        setSupportActionBar(myToolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.options, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if(id == R.id.action_load){
+
+        }
+
+        if(id == R.id.action_save){
+
+        }
+
+        if(id == R.id.action_restart){
+
+        }
+
+        if(id == R.id.action_help){
+            Intent helpIntent = new Intent(MainActivity.this, HelpActivity.class);
+            startActivity(helpIntent);
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
